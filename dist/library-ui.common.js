921c87da3702d54d6130984a19062ab4b0e8dcb8
Jhonnatan Castro <jhonnatan.castro@orbis.com.pe>
2018-05-24 12:50:09 -0500

    feat(config): files builder

--- dist/library-ui.common.js
+++ dist/library-ui.common.js
@@ -3,10 +3,10 @@
 		module.exports = factory();
 	else if(typeof define === 'function' && define.amd)
 		define([], factory);
-	else {
-		var a = factory();
-		for(var i in a) (typeof exports === 'object' ? exports : root)[i] = a[i];
-	}
+	else if(typeof exports === 'object')
+		exports["library-components-vue"] = factory();
+	else
+		root["library-components-vue"] = factory();
 })(typeof self !== 'undefined' ? self : this, function() {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
