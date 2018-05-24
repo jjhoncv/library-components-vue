1b50d41adaf771965aa8625b2b56287f41089b74
Jhonnatan Castro <jhonnatan.castro@orbis.com.pe>
2018-05-24 16:30:35 -0500

    feat(config): files builder

--- components/Button.js
+++ components/Button.js
@@ -1,14 +1,4 @@
-(function webpackUniversalModuleDefinition(root, factory) {
-	if(typeof exports === 'object' && typeof module === 'object')
-		module.exports = factory();
-	else if(typeof define === 'function' && define.amd)
-		define([], factory);
-	else {
-		var a = factory();
-		for(var i in a) (typeof exports === 'object' ? exports : root)[i] = a[i];
-	}
-})(typeof self !== 'undefined' ? self : this, function() {
-return /******/ (function(modules) { // webpackBootstrap
+/******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
 /******/
@@ -9160,4 +9150,3 @@ if (false) {
 
 /***/ })
 /******/ ]);
-});
