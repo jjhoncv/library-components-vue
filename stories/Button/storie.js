707485ff2d60680df69598689d72bb64b16a249c
Jhonnatan Castro <jhonnatan.castro@orbis.com.pe>
2018-05-24 15:40:02 -0500

    feat(config): files builder

--- stories/Button/storie.js
+++
@@ -1,8 +0,0 @@
-// import { storiesOf } from '@storybook/vue';
-// import ButtonDefault from './Button.default';
-
-// import { withKnobs } from '@storybook/addon-knobs';
-
-// storiesOf('Buttons', module)
-//   .addDecorator(withKnobs)
-//   .add('Button Default', ButtonDefault());
