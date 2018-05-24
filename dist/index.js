a8e448e71cdd09f0c32fb2201cf98bdbd60abe22
Jhonnatan Castro <jhonnatan.castro@orbis.com.pe>
2018-05-24 16:30:35 -0500

    feat(config): files builder

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,11 @@
+// import Button from "./Button";
+// import Table from "./Table";
+
+// export default {
+//   Button,
+//   Table
+// };
+
 const Button = require('./components/Button');
 const Table = require('./components/Table');
 
